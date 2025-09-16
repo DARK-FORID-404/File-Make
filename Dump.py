ec497b4e74fe3d2a411ea6163fdd86f43ac2e5e2
DARK-FORID-404 <158121461+DARK-FORID-404@users.noreply.github.com>
2025-09-16 14:34:13 +0600

    Add files via upload

---
+++ Dump.py
@@ -0,0 +1,10 @@
+import os,platform
+os.system('clear')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('YOUR DEVICE 64 BIT')
+    import dump
+elif bit=='32bit':
+    print('YOUR DEVICE 32 BIT ')
+    import dump32
