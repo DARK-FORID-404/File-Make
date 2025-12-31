081e1da5d0b333c63e57e71226589d70ef911354
DARK-FORID-404 <158121461+DARK-FORID-404@users.noreply.github.com>
2025-12-31 11:42:04 +0600

    Update Dump.py

--- Dump.py
+++ Dump.py
@@ -4,7 +4,7 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
     print('YOUR DEVICE 64 BIT')
-    import dump
+    print('64 BIT NOT AVAIABLE NOW')
 elif bit=='32bit':
     print('YOUR DEVICE 32 BIT ')
     import dump
